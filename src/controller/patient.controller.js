02ce3dfed11f5ce9fda9f9a653871df035e81b85
남형석 <azerc@MacMini.local>
2022-09-18 22:24:25 +0900

    add deletePatient function on controller

--- src/controller/patient.controller.js
+++ src/controller/patient.controller.js
@@ -150,4 +150,31 @@ export const updatePatient = (req, res) => {
   });
 };
 
+export const deletePatient = (req, res) => {
+  logger.log(`${req.method} ${req.originalUrl}, deleting patient`);
+  database.query(QUERY.DELETE_PATIENT, [req.params.id], (error, results) => {
+    if (results.affectedRows > 0) {
+      res
+        .status(HttpStatus.OK.code)
+        .send(
+          new Response(
+            HttpStatus.OK.code,
+            HttpStatus.OK.status,
+            `Patient deleted`,
+            results[0]
+          )
+        );
+    }
+    res
+      .status(HttpStatus.NOT_FOUND.code)
+      .send(
+        new Response(
+          HttpStatus.NOT_FOUND.code,
+          HttpStatus.NOT_FOUND.status,
+          `No patient founded by id ${req.params.id}`
+        )
+      );
+  });
+};
+
 export default HttpStatus;
