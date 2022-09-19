e200d7da272d6f8f192357ef075e77e2396a21b0
남형석 <azerc@MacMini.local>
2022-09-19 23:12:03 +0900

    add express.Router();

---
+++ src/route/patient.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getPatients,
+  createPatient,
+  getPatient,
+  deletePatient,
+  updatePatient,
+} from "../controller/patient.controller";
+
+const patientRoutes = express.Router();
+
+patientRoutes.route("/").get(getPatients).post(createPatient);
+patientRoutes
+  .route("/:id")
+  .get(getPatient)
+  .put(updatePatient)
+  .delete(deletePatient);
+
+export default patientRoutes;
